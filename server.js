989a0ad229f8ab7d6e76e4c9b753143920a0f1e8
Aayushh Kartik <aayush.kartik123@gmail.com>
2021-12-24 23:14:07 +0530

    second

--- server.js
+++ server.js
@@ -7,17 +7,19 @@ const cors = require('cors');
 const CustomRouter = require('./Routes/routes.js')
 const app = express();
 
-const corsOptions ={
-    origin:'*', 
-    credentials:true,            //access-control-allow-credentials:true
-    optionSuccessStatus:200,
- }
-app.use(cors(corsOptions))
+// const corsOptions ={
+//     origin:'*', 
+//     credentials:true,            //access-control-allow-credentials:true
+//     optionSuccessStatus:200,
+//  }
+// app.use(cors(corsOptions))
 app.use(cookieParser())
 app.use(express.json());
 mongoose.connect(process.env.DATABASE, {useNewUrlParser: true, useUnifiedTopology: true});
 app.use(CustomRouter);
 
-app.listen(8080, function (req,res) {
+port = process.env.PORT || 8080;
+
+app.listen(port, function (req,res) {
     console.log('listening on port 8080');
 });
